f2677f4a85e6d9f1ba2fed692744de2e397d2e79
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2011-02-02 18:16:35 -0300

    Adding LGPL Licence headers
    Signed-off-by: Daker Fernandes Pinheiro <dakerfp@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+"""
+DSP-Tool
+
+This file is part of the DSP-Tool application.
+
+This application is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+This application is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this application;  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 
 import sys
 from PySide.QtGui import QApplication
--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -1,3 +1,21 @@
+"""
+DSP-Tool
+
+This file is part of the DSP-Tool application.
+
+This application is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+This application is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this application;  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 from PySide.QtCore import Signal, Slot
 from PySide.QtGui import QMainWindow, QVBoxLayout, QMenu, QMenuBar
