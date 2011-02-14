216ed30cf93bc172dc922ae8d227ab080b9d5cae
Héctor Pinheiro <hector@hector-lt.(none)>
2011-02-14 15:55:42 -0200

    Adiciona "Aplicar Operações", completa a implementação "MainWindow"

--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -17,24 +17,47 @@ You should have received a copy of the GNU Lesser General Public License
 along with this application;  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
+import numpy as np
+import scipy as sp
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from PySide.QtCore import Signal, Slot
-from PySide.QtGui import QMainWindow, QVBoxLayout, QMenu, QMenuBar
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from PySide.QtGui import QMainWindow, QVBoxLayout, QMenu, QMenuBar, QFileDialog, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPixmap, QScrollArea
+from Signal import DSPToolSignal
+from .operations import ApplyOperation
 
 class DSPToolFileMenu(QMenu):
     """
     """
     def __init__(self, parent=None):
-        QMenu.__init__(self, self.trUtf8("&Files"))
+        QMenu.__init__(self, "&Files")
         self.parent = parent
 
         newProjectAction = self.addAction(self.trUtf8("&New Project"))
         newProjectAction.triggered.connect(self.createNewProject)
+        
+        openProjectAction = self.addAction(self.trUtf8("&Open Project"))
+        openProjectAction.triggered.connect(self.openProject)
 
         quitAction = self.addAction(self.trUtf8("&Quit Program"))
         quitAction.triggered.connect(self.parent.close)
 
     @Slot()
     def createNewProject(self):
+        """
+        """
+                
+    @Slot()
+    def openProject(self):
+        """
+        """
+        pass
+    
+    @Slot()
+    def saveProject(self):
         """
         """
         pass
@@ -44,16 +67,57 @@ class DSPToolSignalsMenu(QMenu):
     """
     """
     def __init__(self, parent=None):
-        QMenu.__init__(self, self.trUtf8("&Signals"))
+        QMenu.__init__(self, "&Signals")
         self.parent = parent
+        
+        addSignalAction = self.addAction(self.trUtf8("&Add Signal"))
+        addSignalAction.triggered.connect(self.addSignal)
+        
+        applyOperationAction = self.addAction(self.trUtf8("&Apply Operation"))
+        applyOperationAction.triggered.connect(self.applyOperation)    
+    
+    
+    def applyOperation(self):
+        ApplyOperation.applyOperation(self.parent)
+    
+    def addSignal(self):
+        """
+        """
+        filePicker = QFileDialog()
+        fileName = filePicker.getOpenFileName(self, 'Select File')
+        filePicker.destroy()        
+                
+        if fileName:
+            signal = DSPToolSignal(fileName[0])
+            
+            self.parent.signalsList.append(signal)
+            
+            figure = Figure()
+            axes = figure.add_subplot(111)
+            axes.plot(signal.signal_read())
+                     
+            canvas = FigureCanvas(figure)
+            
+            figure.set_figwidth(6)
+            figure.set_figheight(2)
 
+            figure.savefig(signal.name)
+            
+            fig = QLabel()
+            fig.setPixmap(QPixmap(signal.name))
+            fig.setFixedSize(600, 200)               
 
+            
+            layout = self.parent.mainLayout.addWidget(fig)
+            
 class DSPToolMainWindow(QMainWindow):
     """
     """
 
     def __init__(self):
         QMainWindow.__init__(self)
+        
+        self.signalsList = []
 
         menuBar = QMenuBar()
 
@@ -64,5 +128,18 @@ class DSPToolMainWindow(QMainWindow):
         menuBar.addMenu(self.signalMenu)
 
         self.setMenuBar(menuBar)
+        
+        self.mainWidget=QWidget(self)
+        
+      
+        
+        scrollWidget = QScrollArea()
+        scrollWidget.setWidget(self.mainWidget)
+        scrollWidget.setWidgetResizable(True)
+        
+        self.setCentralWidget(scrollWidget)
+        
+        self.mainLayout=QVBoxLayout(self.mainWidget)
+        
 
 
