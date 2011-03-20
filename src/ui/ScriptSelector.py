373dc3ea87a0904e647dcdd1075a4745cc7ea3e4
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2011-03-20 19:18:47 -0300

    Integrates QML selection ui with ScriptSelector
    Signed-off-by: Daker Fernandes Pinheiro <dakerfp@gmail.com>

--- src/ui/ScriptSelector.py
+++ src/ui/ScriptSelector.py
@@ -2,8 +2,10 @@
 from PySide.QtCore import QUrl, Slot
 from PySide.QtDeclarative import QDeclarativeView
 from scripts.script import Module
+from threading import Lock
 
 class ScriptSelector(QDeclarativeView):
+
     def __init__(self, modules_path):
         QDeclarativeView.__init__(self)
         self.module = Module(modules_path)
@@ -11,8 +13,17 @@ class ScriptSelector(QDeclarativeView):
         print self.module.modules
         context.setContextProperty('plugins', self.module.toDict())
         self.setSource(QUrl('ui/qml/ScriptChooser.qml'))
-
+        self.rootObject().scriptSelected.connect(self.onScriptSelected)
+        self.__lock = Lock()
 
     @Slot(int, int)
-    def scriptSelected(self, scriptIndex, ):
-	pass # TODO
+    def onScriptSelected(self, moduleIndex, scriptIndex):
+        self.hide()
+        with self.__lock:
+            if self.callback:
+                self.callback(self.module[moduleIndex][scriptIndex])
+                self.callback = None
+
+    def selectScript(self, callback):
+        self.show()
+        self.callback = callback
